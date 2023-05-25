9b07a3a3ec1715f5644e475c19229baaf370ef84
jqpoon <poonjiaqi@gmail.com>
2023-05-25 13:49:43 +0100

    Fix chart errors for good (hopefully)

--- src/scenes/loading.ts
+++ src/scenes/loading.ts
@@ -1,20 +1,14 @@
 import { Scene } from 'phaser';
-import { Chart } from 'chart.js';
-import annotationPlugin from 'chartjs-plugin-annotation';
 
 export class LoadingScene extends Scene {
   constructor() {
     super('loading-scene');
   }
 
-  preload(): void {
-    this.load.script('chartjs', 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.js');
-    this.load.script('chartjs-plugin-annotation', 'https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/2.2.1/chartjs-plugin-annotation.min.js');
-    Chart.register(annotationPlugin);
-  }
+  preload(): void {}
 
   create(): void {
-    this.scene.start('environment-scene');
-    this.scene.start('ui-scene');
+    this.scene.launch('environment-scene');
+    this.scene.launch('ui-scene');
   }
 }
