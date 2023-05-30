35435ce700898b2ef7b2583738572e333259f9e4
jqpoon <poonjiaqi@gmail.com>
2023-05-30 13:54:38 +0100

    Fix NaN energy loss at the start of simulation

--- src/scenes/ui/mainUI.ts
+++ src/scenes/ui/mainUI.ts
@@ -42,6 +42,7 @@ export class UIScene extends Scene {
     this.bottomPanel.reset();
 
     this.registry.set(REGISTRY_KEYS.worldAge, 0);
+    this.registry.set(REGISTRY_KEYS.energyLoss, 1);
     this.registry.set(REGISTRY_KEYS.organismSpecies, 0);
     this.registry.set(REGISTRY_KEYS.organismColour, 0xe8000b);
     this.registry.set(REGISTRY_KEYS.organismSize, 50);
