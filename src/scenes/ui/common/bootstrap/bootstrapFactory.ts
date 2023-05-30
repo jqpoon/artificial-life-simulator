8afa645e4556aaa3cb8e86e7c6609038e696c676
jqpoon <poonjiaqi@gmail.com>
2023-05-30 13:20:47 +0100

    Add more bootstrap components and add comments

--- src/scenes/ui/common/bootstrap/bootstrapFactory.ts
+++ src/scenes/ui/common/bootstrap/bootstrapFactory.ts
@@ -1,20 +1,23 @@
 type DropdownOption = { [key: string]: string }; // key: 'display text'
 
+/**
+ * Generates a random UUID that IS NOT cryptographically sound but good enough
+ * @returns
+ */
 let quickUUID = () => {
   return Math.random().toString(36).substring(2, 15);
 };
 
 export class BootstrapFactory {
+  /* Common function to create a DOM element and add a click listener */
   private static createDOMElement(
     scene: Phaser.Scene,
     callbackFn: (e: any) => void,
     context: any,
-    htmlString: string,
-    x: number = 0,
-    y: number = 0
+    htmlString: string
   ) {
     /* Add functionality */
-    let element = scene.add.dom(x, y).createFromHTML(htmlString);
+    let element = scene.add.dom(0, 0).createFromHTML(htmlString);
     element.addListener('click');
     element.on('click', callbackFn, context);
 
@@ -24,19 +27,19 @@ export class BootstrapFactory {
   /**
    * Creates a new dropdown menu using bootstrap
    *
-   * @param scene
-   * @param x Optional, x position on the screen
-   * @param y Optional, y position on the screen
+   * @param scene Phaser scene to add this component to
+   * @param callbackFn - Function to call when this element is clicked
+   * @param context - Context of callback function
+   * @param options - Dictionary with format {key: displayText} to be added to
+   *                  this dropdown menu
+   * @returns A dropdown menu DOM object
    */
   public static createDropdown(
     scene: Phaser.Scene,
     callbackFn: (e: any) => void,
     context: any,
-    options: DropdownOption,
-    x: number = 0,
-    y: number = 0
+    options: DropdownOption
   ) {
-    /* Craft bootstrap html */
     let htmlString = `<select class="form-select form-select-lg mb-3">
     `;
 
@@ -50,68 +53,87 @@ export class BootstrapFactory {
       scene,
       callbackFn,
       context,
-      htmlString,
-      x,
-      y
+      htmlString
     );
   }
 
   /**
-   * Creates a new slider using bootstrap
+   * Creates a new slider using bootstrap. This slider will have 100 discrete
+   * steps between min and max, which default to 0 and 100 respectively
    *
-   * @param scene
-   * @param callbackFn - Function to call when
-   * @param context
-   * @param min
-   * @param max
-   * @param x
-   * @param y
-   * @returns
+   * @param scene Phaser scene to add this component to
+   * @param callbackFn - Function to call when this element is clicked
+   * @param context - Context of callback function
+   * @param min - Optional, sets the minimum of this slider
+   * @param max - Optional, sets the maximum of this slider
+   * @returns A slider DOM object
    */
   public static createSlider(
     scene: Phaser.Scene,
     callbackFn: (e: any) => void,
     context: any,
     min: number = 0,
-    max: number = 100,
-    x: number = 0,
-    y: number = 0
+    max: number = 100
   ) {
-    let htmlString = `<input type="range" style="margin: auto; display: block;" class="form-range w-50" min="${min}" max="${max}">`;
+    let htmlString = `<input type="range" style="margin: auto; display: block;" class="form-range w-50" min="${min}" max="${max}" step="${
+      (max + min) / 100
+    }"
+    form-range-track-bg="$gray-500"
+    >`;
+
+    let element = BootstrapFactory.createDOMElement(
+      scene,
+      callbackFn,
+      context,
+      htmlString
+    );
+
+    /* Add additional listener to update values while slider is being dragged, not just at the end of dragging */
+    element.addListener('input');
+    element.on('input', callbackFn, context);
+
+    return element;
+  }
+
+  /**
+   * Creates an (i) icon that displays text when it is moused-over
+   *
+   * @param scene - Phaser scene to add this component to
+   * @param helpText - Help text to display
+   * @returns An icon DOM object
+   */
+  public static createHelpIcon(scene: Phaser.Scene, helpText: string) {
+    let id = quickUUID();
+    let htmlString = `<button type="button" class="btn" data-bs-trigger="hover" data-bs-toggle="tooltip" title="${helpText}" id="${id}">
+      <i class="bi bi-info-circle" style="font-size:24px;"></i>
+      </button>
+      `;
+
+    return BootstrapFactory.createDOMElement(scene, () => {}, null, htmlString);
+  }
+
+  /**
+   * Creates a boostrap button
+   *
+   * @param scene - Phaser scene to add this component to
+   * @param buttonText - Text to display on the button
+   * @param callbackFn - Function to call when this element is clicked
+   * @param context - Context of callback function
+   * @returns A buttom DOM element
+   */
+  public static createButton(
+    scene: Phaser.Scene,
+    buttonText: string,
+    callbackFn: (e: any) => void,
+    context: any
+  ) {
+    let htmlString = `<button type="button" class="btn btn-primary btn-lg">${buttonText}</button>`;
 
     return BootstrapFactory.createDOMElement(
       scene,
       callbackFn,
       context,
-      htmlString,
-      x,
-      y
-    );
-  }
-
-  public static createHelpIcon(
-    scene: Phaser.Scene,
-    helpText: string,
-    x: number = 0,
-    y: number = 0
-  ) {
-    let id = quickUUID();
-    let htmlString = `<button type="button" class="btn btn-primary" data-bs-trigger="hover" data-bs-toggle="tooltip" data-bs-placement="top" title="${helpText}" id="${id}">
-      <i class="bi bi-question-circle"></i>
-      </button>
-      <script>
-        var exampleEl = document.getElementById('${id}')
-        var tooltip = new bootstrap.Tooltip(exampleEl)
-      </script>
-      `;
-
-    return BootstrapFactory.createDOMElement(
-      scene,
-      () => {},
-      null,
-      htmlString,
-      x,
-      y
+      htmlString
     );
   }
 }
