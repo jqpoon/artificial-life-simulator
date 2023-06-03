455e63d4a2a0da365df0310f69d43f7161b0be7e
jqpoon <poonjiaqi@gmail.com>
2023-06-03 10:57:39 +0100

    Add matrix dot product and transpose functions

--- src/classes/neuralNetworks/networkMath.ts
+++ src/classes/neuralNetworks/networkMath.ts
@@ -1,4 +1,7 @@
+import assert from "assert";
+
 export type ActivationFunction = typeof Math.tanh;
+export type ActivationFunctionDerivatives = typeof tanhDerivative;
 export type Initialiser = typeof glorotUniform;
 
 /**
@@ -8,10 +11,14 @@ export type Initialiser = typeof glorotUniform;
  * @param n - Number of inputs (i.e. size of previous layer)
  * @returns A random number drawn from the distribution
  */
-export function glorotUniform(n: number) {
+export function glorotUniform(n: number): number {
   let max = 1 / Math.sqrt(n);
   let min = -max;
   return Math.random() * (max - min) + min;
+}
+
+export function tanhDerivative(x: number): number {
+  return 1 - Math.tanh(x) ** 2;
 }
 
 /**
@@ -58,4 +65,48 @@ export function matrixMultiply(a: number[][], b: number[][]): number[][] {
   }
 
   return result;
+}
+
+/**
+ * Performs matrix dot product. Both matrices must have the same dimensions.
+ *
+ * @param a First matrix, must be 1D
+ * @param b Second matrix, must be 1D
+ * @returns Dot product of both matrices, as a scalar
+ */
+export function matrixDotProduct(a: number[], b: number[]): number {
+  assert(a.length === b.length);
+  const rowsA = a.length;
+
+  let result: number = 0;
+
+  for (let i = 0; i < rowsA; i++) {
+    result += a[i] * b[i];
+  }
+
+  return result;
+}
+
+/**
+ * Transposes a 2D matrix
+ *
+ * @param a Matrix to be transposed
+ * @returns Transposed matrix
+ */
+export function matrixTranspose2D(a: number[][]): number[][] {
+  const rowsA = a.length;
+  const colsA = a[0].length;
+
+  const copy: number[][] = new Array(colsA);
+  for (let i = 0; i < colsA; i++) {
+    copy[i] = new Array(rowsA);
+  }
+
+  for (let i = 0; i < rowsA; i++) {
+    for (let j = 0; j < colsA; j++) {
+      copy[j][i] = a[i][j];
+    }
+  }
+
+  return copy;
 }
--- tests/classes/neuralNetworks/networkMath.test.ts
+++ tests/classes/neuralNetworks/networkMath.test.ts
@@ -1,6 +1,8 @@
 import {
+  matrixDotProduct,
   matrixElementWiseAdd,
   matrixMultiply,
+  matrixTranspose2D,
 } from '../../../src/classes/neuralNetworks/networkMath';
 
 describe('Matrix addition', () => {
@@ -26,5 +28,28 @@ describe('Matrix multiplication', () => {
       [7, 6],
       [17, 16],
     ]);
+  });
+});
+
+describe('Matrix dot product', () => {
+  it('works on 1D arrays', () => {
+    let a = [1, 2, 3];
+    let b = [4, 5, 6];
+    expect(matrixDotProduct(a, b)).toStrictEqual(32);
+  });
+});
+
+describe('Matrix transpose 2D', () => {
+  it('works on 2x3 array', () => {
+    let a = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+    let aT = [
+      [1, 4],
+      [2, 5],
+      [3, 6],
+    ];
+    expect(matrixTranspose2D(a)).toStrictEqual(aT);
   });
 });
