ddb476b10455c9961a467f63d8a89b38716479ea
jqpoon <poonjiaqi@gmail.com>
2023-06-03 11:40:39 +0100

    Implement backprop for neural network

--- src/classes/neuralNetworks/linearLayer.ts
+++ src/classes/neuralNetworks/linearLayer.ts
@@ -2,10 +2,13 @@ import assert from 'assert';
 import { LayerBias, LayerParams, LayerWeights } from './network';
 import {
   ActivationFunction,
+  ActivationFunctionDerivatives,
   Initialiser,
   glorotUniform,
   matrixElementWiseAdd,
   matrixMultiply,
+  matrixTranspose2D,
+  tanhDerivative,
 } from './networkMath';
 
 /**
@@ -19,13 +22,21 @@ import {
 export class LinearLayer {
   private weights: LayerWeights;
   private bias: LayerBias;
+  private weightLoss: LayerWeights;
+  private biasLoss: LayerBias;
+  private inputs: number[];
+  private learningRate: number;
+
   private activationFunction: ActivationFunction;
+  private activationFunctionDerivative: ActivationFunctionDerivatives;
 
   constructor(
     input: number,
     output: number,
     activationFunction: ActivationFunction = Math.tanh,
-    initFunction: Initialiser = glorotUniform
+    activationFunctionDerivative: ActivationFunctionDerivatives = tanhDerivative,
+    initFunction: Initialiser = glorotUniform,
+    learningRate: number = 0.05
   ) {
     /* Initialise weights and bias */
     let weights = Array.from(Array(input), () => new Array(output).fill(0));
@@ -42,6 +53,8 @@ export class LinearLayer {
     this.weights = weights;
     this.bias = bias;
     this.activationFunction = activationFunction;
+    this.activationFunctionDerivative = activationFunctionDerivative;
+    this.learningRate = learningRate;
   }
 
   /**
@@ -51,6 +64,8 @@ export class LinearLayer {
    *          biases and activation function
    */
   public forward(x: number[]): number[] {
+    this.inputs = x; // Store this to use in backprop later
+
     x = matrixMultiply([x], this.weights)[0]; // 0th element since x is a 1D array
     x = matrixElementWiseAdd(x, this.bias);
     x = x.map(this.activationFunction);
@@ -100,6 +115,77 @@ export class LinearLayer {
   public setBias(bias: LayerBias) {
     this.assertBiasValidShape(bias);
     this.bias = bias;
+  }
+
+  /**
+   * Calculates the loss for this layer with reference to its inputs, weights and biases.
+   * Returns the loss with reference to its inputs.
+   *
+   * @param loss Loss from the layer above. Must be the same size as the number
+   *             of output neurons
+   * @returns    Loss with reference to its inputs
+   */
+  public backprop(loss: number[]): number[] {
+    /* Work backwards, input was weights -> bias -> activation
+     * this.weights - input x output
+     * this.inputs  - 1 x input
+     * loss         - 1 x output
+     * biasLoss     - 1 x output
+     * inputsLoss   - 1 x input
+     * weightsLoss  - input x output
+     */
+
+    /* Bias has same size as number of output neurons */
+    assert(
+      loss.length === this.bias.length,
+      `Invalid length of loss passed to backprop in linear layer. Expected ${this.bias.length}, got ${loss.length}`
+    );
+
+    /* Calculate loss wrt to activation function, by using the derivative of the activation function */
+    let activationLoss = loss.map(this.activationFunctionDerivative);
+
+    /* Then calculate loss wrt to bias */
+    let biasLoss = activationLoss; // Since this is just a linear addition, gradient is 1
+
+    /* Calculate loss wrt to weights */
+    let weightsLoss = matrixMultiply(matrixTranspose2D([this.inputs]), [
+      biasLoss,
+    ]);
+
+    /* Calculate loss wrt to inputs */
+    let inputsLoss = matrixMultiply(
+      [biasLoss],
+      matrixTranspose2D(this.weights)
+    );
+
+    /* Store losses to update our weights with later */
+    this.biasLoss = biasLoss;
+    this.weightLoss = weightsLoss;
+
+    return inputsLoss[0];
+  }
+
+  /**
+   * Updates weights using gradient descent. The backpropogation step must be run first.
+   */
+  public updateWeights(): void {
+    assert(this.weightLoss);
+    assert(this.biasLoss);
+
+    let rowsW = this.weights.length;
+    let colsW = this.weights[0].length;
+
+    /* Update weights matrix */
+    for (let i = 0; i < rowsW; i++) {
+      for (let j = 0; j < colsW; j++) {
+        this.weights[i][j] -= this.learningRate * this.weightLoss[i][j];
+      }
+    }
+
+    /* Update bias */
+    for (let i = 0; i < colsW; i++) {
+      this.bias[i] -= this.learningRate * this.biasLoss[i];
+    }
   }
 
   /* Checks shape of weights matrix */
--- tests/classes/neuralNetworks/linearLayer.test.ts
+++ tests/classes/neuralNetworks/linearLayer.test.ts
@@ -28,4 +28,23 @@ describe('Linear layer', () => {
 
     expect(layer.forward([2, 2])).toStrictEqual([Math.tanh(5), Math.tanh(4.5)]);
   });
+
+  it('can backpropogate correctly', () => {
+    let id = (x: number) => {
+      return x;
+    };
+
+    let layer = new LinearLayer(2, 1, id, id);
+
+    layer.setParams({
+      weights: [[0.5], [2]],
+      bias: [2]
+    });
+
+    /* Sanity check to make sure forward pass is okay */
+    expect(layer.forward([2, 3])).toStrictEqual([9]);
+
+    /* Check backprop */
+    expect(layer.backprop([0.5])).toStrictEqual([0.25, 1]);
+  });
 });
