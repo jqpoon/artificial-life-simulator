000b5f0bee55501478de52577fc9aa269db8fc64
jqpoon <poonjiaqi@gmail.com>
2023-06-14 11:48:20 +0100

    Set default mutation rate to be 0.05

--- src/scenes/ui/sidePanel/simulatorTab/simulatorTab.ts
+++ src/scenes/ui/sidePanel/simulatorTab/simulatorTab.ts
@@ -21,7 +21,7 @@ export class SimulatorTab extends TabComponent {
     this.scenarioControl = new ScenarioControl(scene);
 
     /* Controls global mutation rate */
-    let mutationText = scene.add.text(0, 0, '0.10', smallerTextDark);
+    let mutationText = scene.add.text(0, 0, '0.05', smallerTextDark);
     this.mutationRateSlider = BootstrapFactory.createSlider(
       scene,
       (e: any) => {
@@ -132,6 +132,6 @@ export class SimulatorTab extends TabComponent {
   reset(): void {
     this.scenarioControl.reset();
     (this.energyLossSlider.node.children[0] as any).value = 1;
-    (this.mutationRateSlider.node.children[0] as any).value = 0.1;
+    (this.mutationRateSlider.node.children[0] as any).value = 0.05;
   }
 }
